76fcb3b58b163732c2a04c34a482e6b83a122da1
caalma <nds.suscript@gmail.com>
2021-07-09 00:34:46 -0300

    Agregado de custom settings.

--- public_html/js/init.js
+++ public_html/js/init.js
@@ -1,20 +1,26 @@
+/* underscore custom settings */
+_.templateSettings = {
+  interpolate:/\{\{=([\s\S]+?)\}\}/g,
+  evaluate:/\{\{([\s\S]+?)\}\}/g,
+  escape:/\{\{--([\s\S]+?)\}--\}/g
+};
+
+
 /* global variable */
 var G = {};
+
 
 /* initialization */
 $(window).on({
 	load: function(){
 		yml_load('cfg/general.yml', function(v){
 			G.cfg = v;
+			
 			$('title').text(G.cfg['title']);
 			$('#title').html(G.cfg['title']);
 			$('#content').html(marked.parse(G.cfg['content']));
-			
-			$('#exit').on({
-				click: function(ev){
-					server_shutdown();
-				}
-			});
+
+			close_server('#exit');
 		});
 	},
 });
